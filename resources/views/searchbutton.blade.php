674b896c08c84cebfe55c527b7eb9348146c7a1a
alam6160 <alam15-6160@diu.edu.bd>
2021-06-11 05:30:04 +0600

    this is first comment

---
+++ resources/views/searchbutton.blade.php
@@ -0,0 +1,10 @@
+@extends("index")
+@section("index-content")
+<div class="search-container">
+           <form action="/action_page.php">
+             <input type="text" placeholder="Search.." name="search">
+                 <button type="submit"><i class="fa fa-search"></i></button>
+                  </form>
+               </div>
+         
+@show
